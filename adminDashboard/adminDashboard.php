52826c80ae4abd8aaa16f855fbfb5a1210becf29
PaulinoNze <153314086+PaulinoNze@users.noreply.github.com>
2024-02-17 14:30:14 +0000

    J'ai déjà réalisé le tableau de bord étudiant mais les fonctionnalités ne sont pas encore là, seulement les interfaces

--- adminDashboard/adminDashboard.php
+++ adminDashboard/adminDashboard.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>Preschool - Bootstrap Admin Template</title>
+<title>adminDashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 
-<link rel="shortcut icon" type="image/x-icon" href="../assets/img/favicon.png">
+<link rel="shortcut icon" type="image/x-icon" href="../Img/logo1.jpg">
 
 <link href="../../../../../css?family=Roboto:300,400,500,700,900" rel="stylesheet">
 
